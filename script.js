335f089b8ba993e725554a6c022800237d2bc46a
Br0cod3 <thespammingbott@gmail.com>
2025-08-21 12:46:25 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const sections = []
+const Library = []
+
+function Book() {
+    
+}
+
+function addBookToLibrary(title, author, numberOfPages) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    Book()
+}
